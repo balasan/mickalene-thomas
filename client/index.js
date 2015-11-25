de7a352c4944dec6db614953ee501b8ba431a2fa
Slava 4REAL <slava@4real.io>
2015-11-25 00:29:26 -0600

    edit

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import configureStore from '../common/store/configureStore'
 import App from '../common/containers/App'
 
-import './css/index.css'
+import './css/index.scss'
 
 const initialState = window.__INITIAL_STATE__
 const store = configureStore(initialState)
