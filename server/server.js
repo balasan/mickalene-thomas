a34a8266709ed8e46ef956374db65b4a3db82cac
Slava 4REAL <slava@4real.io>
2015-12-29 15:24:16 -0500

    wtf

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ console.log(process.env.NODE_ENV)
 //-------------Dev server watch and hot reload---------------
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 if (isDevelopment) {
+  console.log("DEV ENV!?!?");
   var webpack = require('webpack');
   var webpackDevMiddleware = require('webpack-dev-middleware');
   var webpackHotMiddleware = require('webpack-hot-middleware');
