1b9c88da06c5ccf1b0765aceb226e7fc523308f6
jaygoss <jaygoss@gmail.com>
2016-02-05 15:39:53 -0500

    remove nav m on mobile

--- common/components/Product.js
+++ common/components/Product.js
@@ -35,12 +35,12 @@ export default class Product extends Component {
 
            <section className='top'>
              <h1>{product.title}</h1>
-             <p className='price'>{product.price ? '$' : null}{product.price ? product.price.toFixed(2) : 'price available upon request'}</p>
+             <p className={product.price ? 'price' : 'price no-price'}>{product.price ? '$' : null}{product.price ? product.price.toFixed(2) : 'price available upon request'}</p>
            </section>
 
            <section className='bottom'>
            <div className='description'><p>{product.description}</p></div>
-            {product.price ? <button onClick={add.bind(this, product)}>Add to Shopping Bag</button> : <a href="mailto:hello@mickalenethomas.com" target="_blank" className="no-price">contact us</a>}
+            {product.price ? <button onClick={add.bind(this, product)}>Add to Shopping Bag</button> : <a href="mailto:hello@mickalenethomas.com" target="_blank" className="no-price">Contact Us</a>}
            </section>
             </div>
         </section>
